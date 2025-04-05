b5c9e5a55363e7c4aa6a3e3bad74c14cf994ba68
Your Name <you@example.com>
2025-04-05 12:55:03 -0400

    ads

--- components/Adsense.jsx
+++ components/Adsense.jsx
@@ -21,11 +21,13 @@ export default function Adsense() {
   }, []);
 
   return (
-<ins class="adsbygoogle"
-     style="display:block"
+     <ins
+     className="adsbygoogle"
+     style={{ display: "block" }}
      data-ad-client="ca-pub-1803218881232491"
      data-ad-slot="6880003388"
      data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
+     data-full-width-responsive="true"
+   ></ins>
   );
 }
